9d011b2372bcf448cd9fd31831ac458a78eeae70
Tatsuro <contacts@tatsuro.dev>
2020-11-12 04:04:33 +0900

    EventForm styling 22

--- revents/src/features/events/eventDashboard/EventDashboard.jsx
+++ revents/src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Grid } from 'semantic-ui-react'
 import EventList from './EventList'
+import EventForm from '../eventForm/EventForm'
 
 export default function EventDashboard() {
   return (
@@ -10,7 +11,7 @@ export default function EventDashboard() {
         <EventList />
       </Grid.Column>
       <Grid.Column width={6}>
-        <h2>Right Column</h2>
+        <EventForm />
       </Grid.Column>
     </Grid>
   )
