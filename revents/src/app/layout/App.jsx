d0d4d67ee1477ba1e367edfff08bc4188c75271b
Tatsuro <contacts@tatsuro.dev>
2020-11-11 11:16:36 +0900

    jsx intro 12

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -1,3 +1,17 @@
+import React from 'react'
+
+// pure javascript code
+/*
+function App() {
+  const title = React.createElement('h1', {}, 'Revents no JSX')
+  const div = React.createContext('div', {className: 'App'}, title)
+  return (
+    div
+  )
+}
+*/
+
+// jsx code
 function App() {
   return (
     <div className="App">
