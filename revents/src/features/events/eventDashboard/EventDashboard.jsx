6625250d01ed3b054576ee321616bb5c208a2491
Tatsuro <contacts@tatsuro.dev>
2020-11-13 04:55:48 +0900

    controlled component 30

--- revents/src/features/events/eventDashboard/EventDashboard.jsx
+++ revents/src/features/events/eventDashboard/EventDashboard.jsx
@@ -29,6 +29,7 @@ export default function EventDashboard({
             setEvents={setEvents}
             createEvent={handleCreateEvent}
             selectedEvent={selectedEvent}
+            key={selectedEvent ? selectedEvent.id : null}
           />
         )}
       </Grid.Column>
