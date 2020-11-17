d58e73ea42f401d9c64b79af8e9d4e91dc0e9139
Tatsuro <contacts@tatsuro.dev>
2020-11-17 13:32:53 +0900

    不要なcodeのclearn up 45

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Route } from 'react-router-dom'
 import { Container } from 'semantic-ui-react'
 import NavBar from '../../features/nav/NavBar'
@@ -20,20 +20,6 @@ function App() {
 
 // jsx code
 export default function App() {
-  // NavBarとEventDashboardの子EventFormでformOpen stateを使用するので、その両方へprop down可能なAppでstateを管理する
-  const [formOpen, setFormOpen] = useState(false)
-  const [selectedEvent, setSelectedEvent] = useState(null)
-
-  function handleSelectEvent(event) {
-    setSelectedEvent(event)
-    setFormOpen(true)
-  }
-
-  function handleCreateFormOpen() {
-    setSelectedEvent(null)
-    setFormOpen(true)
-  }
-
   return (
     <>
       <Route exact path="/" component={HomePage} />
@@ -41,12 +27,15 @@ export default function App() {
         path={'/(.+)'}
         render={() => (
           <>
-            <NavBar setFormOpen={handleCreateFormOpen} />
+            <NavBar />
             <Container className="main">
               <Route exact path="/" component={HomePage} />
               <Route exact path="/events" component={EventDashboard} />
               <Route path="/events/:id" component={EventDetailedPage} />
-              <Route path="/createEvent" component={EventForm} />
+              <Route
+                path={['/createEvent', '/manage/:id']}
+                component={EventForm}
+              />
             </Container>
           </>
         )}
--- revents/src/features/events/eventDashboard/EventDashboard.jsx
+++ revents/src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,27 +1,20 @@
 import React, { useState } from 'react'
 import { Grid } from 'semantic-ui-react'
 import EventList from './EventList'
-import EventForm from '../eventForm/EventForm'
 import { sampleData } from '../../../app/api/sampleData'
 
-export default function EventDashboard({
-  formOpen,
-  setFormOpen,
-  selectEvent,
-  selectedEvent,
-}) {
+export default function EventDashboard() {
   const [events, setEvents] = useState(sampleData)
 
-  function handleCreateEvent(event) {
-    setEvents([...events, event])
-  }
+  // function handleCreateEvent(event) {
+  //   setEvents([...events, event])
+  // }
 
-  function handleUpdateEvent(updateEvent) {
-    setEvents(
-      events.map((evt) => (evt.id === updateEvent.id ? updateEvent : evt))
-    )
-    selectEvent(null)
-  }
+  // function handleUpdateEvent(updateEvent) {
+  //   setEvents(
+  //     events.map((evt) => (evt.id === updateEvent.id ? updateEvent : evt))
+  //   )
+  // }
 
   function handleDeleteEvent(eventId) {
     setEvents(events.filter((evt) => evt.id !== eventId))
@@ -31,23 +24,10 @@ export default function EventDashboard({
     <Grid>
       {/* semantic uiは、16 columns grid */}
       <Grid.Column width={10}>
-        <EventList
-          events={events}
-          selectEvent={selectEvent}
-          deleteEvent={handleDeleteEvent}
-        />
+        <EventList events={events} deleteEvent={handleDeleteEvent} />
       </Grid.Column>
       <Grid.Column width={6}>
-        {formOpen && (
-          <EventForm
-            setFormOpen={setFormOpen}
-            setEvents={setEvents}
-            createEvent={handleCreateEvent}
-            selectedEvent={selectedEvent}
-            updateEvent={handleUpdateEvent}
-            key={selectedEvent ? selectedEvent.id : null}
-          />
-        )}
+        <h2>Event Filters</h2>
       </Grid.Column>
     </Grid>
   )
