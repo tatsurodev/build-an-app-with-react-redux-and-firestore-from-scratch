904c04376a33ddd85d6f2d2f32863f258f17eda1
Tatsuro <contacts@tatsuro.dev>
2020-11-18 09:57:33 +0900

    action objectの代わりにaction creatorでdispatch 52

--- revents/src/features/sandbox/Sandbox.jsx
+++ revents/src/features/sandbox/Sandbox.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Button } from 'semantic-ui-react'
-import { DECREMENT_COUNTER, INCREMENT_COUNTER } from './testReducer'
+import { decrement, increment } from './testReducer'
 
 export default function Sandbox() {
   const dispatch = useDispatch()
@@ -12,12 +12,12 @@ export default function Sandbox() {
       <h1>Testing 123</h1>
       <h3>The data is : {data}</h3>
       <Button
-        onClick={() => dispatch({ type: INCREMENT_COUNTER })}
+        onClick={() => dispatch(increment(20))}
         content="Increment"
         color="green"
       />
       <Button
-        onClick={() => dispatch({ type: DECREMENT_COUNTER })}
+        onClick={() => dispatch(decrement(10))}
         content="Decrement"
         color="red"
       />
