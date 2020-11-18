d6dfc4b258b6b7b0e9ff73629859d4a2a3021537
Tatsuro <contacts@tatsuro.dev>
2020-11-18 09:14:44 +0900

    redux install, setup 49

---
+++ revents/src/features/sandbox/testReducer.js
@@ -0,0 +1,7 @@
+const initialStae = {
+  data: 42,
+}
+
+export default function testReducer(state = initialStae) {
+  return state
+}
--- revents/src/index.js
+++ revents/src/index.js
@@ -1,18 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css'
 import App from './app/layout/App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
+import { configureStore } from './app/store/configureStore'
+
+const store = configureStore()
+
+console.log(store.getState())
 
 const rootEl = document.getElementById('root')
 
 function render() {
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
     rootEl
   )
 }
