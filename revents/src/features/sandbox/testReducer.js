c3a18ca2a6934a23746869f70955ca5dda7f3bd0
Tatsuro <contacts@tatsuro.dev>
2020-11-18 09:51:01 +0900

    reducerの作成、actionをdispatch 51

--- revents/src/features/sandbox/testReducer.js
+++ revents/src/features/sandbox/testReducer.js
@@ -1,7 +1,23 @@
+export const INCREMENT_COUNTER = 'INCREMENT_COUNTER'
+export const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
+
 const initialStae = {
   data: 42,
 }
 
-export default function testReducer(state = initialStae) {
-  return state
+export default function testReducer(state = initialStae, action) {
+  switch (action.type) {
+    case INCREMENT_COUNTER:
+      return {
+        ...state,
+        data: state.data + 1,
+      }
+    case DECREMENT_COUNTER:
+      return {
+        ...state,
+        data: state.data - 1,
+      }
+    default:
+      return state
+  }
 }
