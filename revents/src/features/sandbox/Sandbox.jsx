af2380f99a22b50f2b607a01c4ae3cb875752af5
Tatsuro <contacts@tatsuro.dev>
2020-11-22 17:50:10 +0900

    react-places-autocomplete install, setup 80

--- revents/src/features/sandbox/Sandbox.jsx
+++ revents/src/features/sandbox/Sandbox.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Button } from 'semantic-ui-react'
 import { openModal } from '../../app/common/modals/modalReducer'
+import TestPlaceInput from './TestPlaceInput'
 import { decrement, increment } from './testReducer'
 
 export default function Sandbox() {
@@ -29,6 +30,9 @@ export default function Sandbox() {
         content="Open Modal"
         color="teal"
       />
+      <div style={{ marginTop: 15 }}>
+        <TestPlaceInput />
+      </div>
     </>
   )
 }
