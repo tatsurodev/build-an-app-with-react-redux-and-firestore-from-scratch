c040e51554c83a34d5bb97d7dbd1c677b5a3ec13
Tatsuro <contacts@tatsuro.dev>
2020-11-29 01:38:38 +0900

    increment, decrementのloadingをそれぞれ分離 91

--- revents/src/features/sandbox/Sandbox.jsx
+++ revents/src/features/sandbox/Sandbox.jsx
@@ -5,10 +5,10 @@ import { openModal } from '../../app/common/modals/modalReducer'
 import TestPlaceInput from './TestPlaceInput'
 import TestMap from './TestMap'
 import { decrement, increment } from './testReducer'
-import { latLng2Tile } from 'google-map-react'
 
 export default function Sandbox() {
   const dispatch = useDispatch()
+  const [target, setTarget] = useState(null)
   const data = useSelector((state) => state.test.data)
   const { loading } = useSelector((state) => state.async)
   const defaultProps = {
@@ -31,14 +31,22 @@ export default function Sandbox() {
       <h1>Testing 123</h1>
       <h3>The data is : {data}</h3>
       <Button
-        loading={loading}
-        onClick={() => dispatch(increment(20))}
+        name="increment"
+        loading={loading && target === 'increment'}
+        onClick={(e) => {
+          dispatch(increment(20))
+          setTarget(e.target.name)
+        }}
         content="Increment"
         color="green"
       />
       <Button
-        loading={loading}
-        onClick={() => dispatch(decrement(10))}
+        name="decrement"
+        loading={loading && target === 'decrement'}
+        onClick={(e) => {
+          dispatch(decrement(10))
+          setTarget(e.target.name)
+        }}
         content="Decrement"
         color="red"
       />
