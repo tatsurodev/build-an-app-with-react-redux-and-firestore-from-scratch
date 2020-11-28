a69970fb0ccfcc1173136c02e0326b4b915234b4
Tatsuro <contacts@tatsuro.dev>
2020-11-28 15:21:24 +0900

    inputで入力した場所をmapで表示 85

--- revents/src/features/sandbox/Sandbox.jsx
+++ revents/src/features/sandbox/Sandbox.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Button } from 'semantic-ui-react'
 import { openModal } from '../../app/common/modals/modalReducer'
 import TestPlaceInput from './TestPlaceInput'
 import TestMap from './TestMap'
 import { decrement, increment } from './testReducer'
+import { latLng2Tile } from 'google-map-react'
 
 export default function Sandbox() {
   const dispatch = useDispatch()
   const data = useSelector((state) => state.test.data)
+  const defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33,
+    },
+    zoom: 11,
+  }
+  const [location, setLocation] = useState(defaultProps)
 
+  function handleSetLocation(latLng) {
+    setLocation({
+      ...location,
+      center: { lat: latLng.lat, lng: latLng.lng },
+    })
+  }
   return (
     <>
       <h1>Testing 123</h1>
@@ -32,8 +47,8 @@ export default function Sandbox() {
         color="teal"
       />
       <div style={{ marginTop: 15 }}>
-        <TestPlaceInput />
-        <TestMap />
+        <TestPlaceInput setLocation={handleSetLocation} />
+        <TestMap location={location} />
       </div>
     </>
   )
