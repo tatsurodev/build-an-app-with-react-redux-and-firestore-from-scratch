83946f6f65b8fa5da7e5e18031236ec16a3c2760
Tatsuro <contacts@tatsuro.dev>
2020-11-30 03:33:26 +0900

    snapshotでfirestoreをlistening 101

--- revents/src/features/events/eventDashboard/EventDashboard.jsx
+++ revents/src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { Grid } from 'semantic-ui-react'
 import EventList from './EventList'
 import EventListItemPlaceholder from './EventListItemPlaceholder'
 import EventFilters from './EventFilters'
+import { getEventsFromFirestore } from '../../../app/firestore/firestoreService'
 
 export default function EventDashboard() {
   const { events } = useSelector((state) => state.event)
   const { loading } = useSelector((state) => state.async)
+
+  useEffect(() => {
+    const unsubscribe = getEventsFromFirestore({
+      next: (snapshot) =>
+        console.log(snapshot.docs.map((docSnapshot) => docSnapshot.data())),
+      error: (error) => console.log(error),
+    })
+    return unsubscribe
+  })
 
   return (
     <Grid>
