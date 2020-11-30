188a763ebe6144e44203d40ad42b8aba1b655479
Tatsuro <contacts@tatsuro.dev>
2020-11-30 21:25:17 +0900

    eventsをfirestoreからfetch 103

--- revents/src/features/events/eventDashboard/EventDashboard.jsx
+++ revents/src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,23 +1,41 @@
 import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Grid } from 'semantic-ui-react'
 import EventList from './EventList'
 import EventListItemPlaceholder from './EventListItemPlaceholder'
 import EventFilters from './EventFilters'
-import { getEventsFromFirestore } from '../../../app/firestore/firestoreService'
+import {
+  dataFromSnapshot,
+  getEventsFromFirestore,
+} from '../../../app/firestore/firestoreService'
+import { listenToEvents } from '../eventActions'
+import {
+  asyncActionError,
+  asyncActionFinish,
+  asyncActionStart,
+} from '../../../app/async/asyncReducer'
 
 export default function EventDashboard() {
+  const dispatch = useDispatch()
   const { events } = useSelector((state) => state.event)
   const { loading } = useSelector((state) => state.async)
 
   useEffect(() => {
+    dispatch(asyncActionStart())
     const unsubscribe = getEventsFromFirestore({
-      next: (snapshot) =>
-        console.log(snapshot.docs.map((docSnapshot) => docSnapshot.data())),
-      error: (error) => console.log(error),
+      next: (snapshot) => {
+        dispatch(
+          listenToEvents(
+            snapshot.docs.map((docSnapshot) => dataFromSnapshot(docSnapshot))
+          )
+        )
+        dispatch(asyncActionFinish())
+      },
+      error: (error) => dispatch(asyncActionError(error)),
+      complete: () => console.log('you will never see this message'),
     })
     return unsubscribe
-  })
+  }, [dispatch])
 
   return (
     <Grid>
